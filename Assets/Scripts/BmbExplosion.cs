28c5acdf5cf847cd0107e697a7884e1da3d5bea5
Mario <48070830+mjcarranza@users.noreply.github.com>
2020-11-22 10:09:51 -0600

    Add files via upload

---
+++ Assets/Scripts/BmbExplosion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BmbExplosion : MonoBehaviour
+{
+    public float bmbLife;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, bmbLife);
+    }
+    public void Explosion()
+    {
+        int i; // cambiar esto por una expresion para que se destruya la bomba y se pongan fuegos y luego desaparezcan
+    }
+}
---
+++ Assets/Scripts/PlyerControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyerControl : MonoBehaviour
+{
+    public float timer;
+    bool firstBmb = true;
+    public Transform bombSpawner;
+    public GameObject bombPrefav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Player`s movement
+        if (Input.GetKey("left"))
+        {
+            gameObject.transform.Translate(-2f * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(2f * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("up"))
+        {
+            gameObject.transform.Translate(0, 2f * Time.deltaTime, 0);
+        }
+        if (Input.GetKey("down"))
+        {
+            gameObject.transform.Translate(0, -2f * Time.deltaTime, 0);
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            if (firstBmb == true)
+            {
+                playerBmb();
+                firstBmb = false;
+            }
+            else
+            {
+                if (timer >= 2)
+                {
+                    playerBmb();
+                    timer = 0;
+                }
+            }
+        }
+        timer += Time.deltaTime;
+        
+    }
+    public void playerBmb()
+    {
+        Instantiate(bombPrefav, bombSpawner.position, bombSpawner.rotation);
+    }
+}
