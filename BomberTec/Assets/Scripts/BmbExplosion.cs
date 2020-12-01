6c8c6c8672dd7427df6249354a8fc4bffee168e3
Mario <48070830+mjcarranza@users.noreply.github.com>
2020-11-30 19:22:49 -0600

    Updating directories with gitignore

---
+++ BomberTec/Assets/Scripts/BmbExplosion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BmbExplosion : MonoBehaviour
+{
+    public float bmbLife;
+    public float fireLife;
+    public bool active = false;
+    public GameObject ExplosionPrefav;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bmbLife += Time.deltaTime;
+        fireLife += Time.deltaTime;
+        if (bmbLife >= 2 && bmbLife < 3)
+        {
+            Destroy(gameObject, bmbLife - 1);
+            //Instantiate(ExplosionPrefav, transform.position, transform.rotation);
+            active = true;
+        }
+        if (active == true)
+        {
+            if(fireLife >= 3 && fireLife < 4)
+            {
+                AutoExplode();
+            }
+        }
+    }
+    void AutoExplode()
+    {
+        Instantiate(ExplosionPrefav, transform.position, transform.rotation);
+        active = false;
+    }
+}
---
+++ BomberTec/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public int enemyHealth;
+    public int enemyValue;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Explosion")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
